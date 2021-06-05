c62a754d66d62a1d0107c07aaa7055fd173ac492
srini <srini@DESKTOP-KTIPF79>
2021-06-05 10:00:20 +0530

    moved to interviews repo

--- src/com/examples/Test.java
+++ src/com/examples/Test.java
@@ -1,10 +1,5 @@
 package com.examples;
 
 public class Test {
-    static String str = "abc";
-    public static void main(String[] args) {
-        str = "abcd";
-        Test test = new Test();
-        System.out.println(test.str);
-    }
+    public static void main(String[] args) {}
 }
